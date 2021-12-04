f231dec91a18cf7d4610f39e318316eaee6bb911
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2021-12-04 21:50:37 +0100

    Recipes displayed correctly; HTML and CSS finished (almost)

--- js/main.js
+++ js/main.js
@@ -2,32 +2,30 @@ import { recipes } from "./recipes.js";
 
 const result = document.querySelector(".cards");
 
-// let ingredients = "\n";
-// for (let recipe of recipes) {
-//   console.log(typeof recipe.ingredients[0].ingredient);
-//   ingredients += `<p class="ingredient">${recipe.ingredients[0].ingredient}</p>`;
-// }
-
-let ingredients = "";
-
-recipes.forEach((recipe) => {
+const createRecipeElem = (recipe) => {
   const recipeHtml = document.createElement("div");
   recipeHtml.classList.add("recipe");
+
+  let pIngredients = "";
+
+  for (let ingredient of recipe["ingredients"]) {
+    pIngredients += `<div class="ingredient"><span class="ingredient__name">${ingredient.ingredient}:</span> ${ingredient.quantity} ${ingredient.unit}</div>`;
+  }
+
   recipeHtml.innerHTML = `<div class="image"></div>
-                            <h2>${recipe.name}</h2>
-                            <i class="far fa-clock"></i><h2>${recipe.time} min</h2>
-                            <div class="ingredients">${ingredients}</div>`;
+                          <div class="text">
+                            <div class="recipe-header">
+                              <h2>${recipe.name}</h2>
+                              <div class="time"><i class="far fa-clock"></i><h2>${recipe.time} min</h2></div>
+                            </div>
+                            <div class="recipe-core">
+                              <div class="ingredients">${pIngredients}</div>
+                              <p class"description">${recipe.description}</p>
+                            </div>
+                          </div>`;
+
+  // return recipeHtml;
   result.appendChild(recipeHtml);
-});
+};
 
-ingredients = document.querySelector(".ingredients");
-
-recipes.forEach((recipe) => {
-  recipe.ingredients.forEach((ingredient) => {
-    console.log(ingredient);
-    const ingredientHtml = document.createElement("p");
-    ingredientHtml.classList.add("ingredient");
-    ingredientHtml.innerHTML = `${ingredient.ingredient} ${ingredient.quantity} ${ingredient.unit}`;
-    ingredients.appendChild(ingredientHtml);
-  });
-});
+recipes.forEach((recipe) => createRecipeElem(recipe));
