03b9c2c440f84049f86f1041889f5551198e8419
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2021-12-08 10:38:41 +0100

    Global sorting finished; class names changed

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 import { recipes } from "./recipes.js";
 
 const result = document.querySelector(".cards");
-const searchInputGlobal = document.getElementById("searchInput");
+const searchInputGlobal = document.getElementById("search-global-input");
 
-let recipeHtml = "";
-
+// function which creates HTML
 const createRecipeElem = (recipe) => {
-  recipeHtml = document.createElement("div");
+  let recipeHtml = document.createElement("div");
   recipeHtml.classList.add("recipe");
 
   let pIngredients = "";
 
+  // loop through ingredients
   for (let ingredient of recipe.ingredients) {
     pIngredients += `<div class="ingredient"><span class="ingredient__name">${
       ingredient.ingredient
@@ -32,25 +32,45 @@ const createRecipeElem = (recipe) => {
                           </div>`;
 
   result.appendChild(recipeHtml);
-  return recipeHtml;
 };
 
+// display recipes
 recipes.forEach((recipe) => createRecipeElem(recipe));
 
+// listen to the input in the global search and sort recipes corresponding to that input
 searchInputGlobal.addEventListener("keyup", (e) => {
-  if (searchInput.validity.valid) {
+  // check if at least 3 characters have been entred in the input field
+  if (searchInputGlobal.validity.valid) {
     console.log("is valid");
 
+    // get the user's input value
     const inputGlobalValue = searchInputGlobal.value.toLowerCase();
     console.log(inputGlobalValue);
 
-    let include = true;
+    // empty the gallery
     result.innerHTML = "";
+    let isIncluded = false;
+
+    // loop through recipes
     recipes.forEach((recipe) => {
-      include =
+      let arrayOfBooleans = [];
+      console.log(recipe.id, arrayOfBooleans);
+
+      // loop through ingredients
+      recipe.ingredients.forEach((ingredient) => {
+        arrayOfBooleans.push(
+          ingredient.ingredient.toLowerCase().includes(inputGlobalValue)
+        );
+      });
+
+      isIncluded =
         recipe.name.toLowerCase().includes(inputGlobalValue) ||
-        recipe.description.toLowerCase().includes(inputGlobalValue);
-      if (include === true) {
+        recipe.description.toLowerCase().includes(inputGlobalValue) ||
+        arrayOfBooleans.some((item) => item === true);
+
+      console.log(arrayOfBooleans.some((item) => item === true));
+
+      if (isIncluded === true) {
         console.log("ID recipe to show", recipe.id);
         createRecipeElem(recipe);
       }
